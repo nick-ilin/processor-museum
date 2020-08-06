e5465a3cc3abdec41c7f87132e056a622b0b9676
nick-ilin <67784318+nick-ilin@users.noreply.github.com>
2020-08-06 13:05:29 +0300

    Made VendorTree separate component

--- src/components/vendor-tree.js
+++ src/components/vendor-tree.js
@@ -2,19 +2,18 @@ import React from "react";
 import TreeView from 'react-treeview';
 import _ from "lodash";
 
-import Data from '../pages/processors/processors.json'
-
-const vendors = _(Data).map('vendor').uniq().value();
+let vendors;
 
 class VendorTree extends React.Component {
     constructor(props) {
         super(props);
+        vendors = Object.keys(this.props.treeData).map((key) => key);
         this.state = {
             collapsedBookKeeping: vendors.map(() => true)
         }
     }
-        
-    handleClick(i) {
+
+    handleClick(i) { 
         this.setState(prevState => {
             let [...collapsedBookKeeping] = prevState.collapsedBookKeeping;
             collapsedBookKeeping[i] = !collapsedBookKeeping[i];
@@ -25,14 +24,14 @@ class VendorTree extends React.Component {
     renderVendorTree(vendor, i) {
         const collapsedBookKeeping = this.state.collapsedBookKeeping;
         const label = <span className="vendorName" onClick={this.handleClick.bind(this, i)}>{vendor}</span>;
-        const devices = _.filter(Data, {vendor: vendors[i]});
+        const devices = this.props.treeData[vendor].map((item) => item)
         return (
             <TreeView key={i} nodeLabel={label} collapsed={collapsedBookKeeping[i]} onClick={this.handleClick.bind(this, i)}>
                 {devices.map(device => <div className="deviceName" key={device.id}><a>{device.name}</a></div>)}
             </TreeView>
         );
     }
-        
+
     render() {
         return (
             <div>{vendors.map((vendor, i) => this.renderVendorTree(vendor, i))}</div>
--- src/pages/processors.js
+++ src/pages/processors.js
@@ -1,12 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import _ from "lodash";
 
 import VendorTree from '../components/vendor-tree';
 
+import Data from '../pages/processors/processors.json';
+
 import '../styles/tree.css';
 
-export default props => (
-    <div>
-        <VendorTree name = '../pages/processors/processors.json' />
-        {props.children}
-    </div>
-);
+const processors = _.groupBy(Data, 'vendor');
+
+class Processors extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            treeData: processors
+        }
+    }
+    render() {
+        return (
+            <div>
+                <VendorTree treeData={this.state.treeData}/>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default Processors;
