09bee0c09c49bd15ab7735b0c9ac37bcd15d5245
nick-ilin <67784318+nick-ilin@users.noreply.github.com>
2021-04-05 10:31:25 +0300

    line length reduced to 120

--- src/components/processor-info.js
+++ src/components/processor-info.js
@@ -7,8 +7,12 @@ const ProcessorInfo = (props) => {
   const info = getProcessorById(props.id);
   return (
     <div className="processorInfo">
-      <a href={info.imageUrlBigTop} target="_blank" rel="noreferrer"><img className="image" src={info.imageUrlTop} alt={info.name} /></a>
-      <a href={info.imageUrlBigBottom} target="_blank" rel="noreferrer"><img className="image" src={info.imageUrlBottom} alt={info.name} /></a>
+      <a href={info.imageUrlBigTop} target="_blank" rel="noreferrer">
+        <img className="image" src={info.imageUrlTop} alt={info.name} />
+      </a>
+      <a href={info.imageUrlBigBottom} target="_blank" rel="noreferrer">
+        <img className="image" src={info.imageUrlBottom} alt={info.name} />
+      </a>
       <div className="description">
         <ul>
           <li>{`Производитель: ${info.vendor}`}</li>
