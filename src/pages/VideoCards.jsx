fc94ca8c8105f50500cb5251710864548966cda1
nick-ilin <67784318+nick-ilin@users.noreply.github.com>
2026-06-12 12:00:23 +0300

    Csv parsing (#65)
    * Create new id
    
    * Foto name as Id
    
    * Eslint
    
    * Add filtered device count
    
    ---------
    
    Co-authored-by: Ильин Николай Сергеевич <nilin@uni-systems.ru>

--- src/pages/VideoCards.jsx
+++ src/pages/VideoCards.jsx
@@ -27,6 +27,8 @@ export default function VideoCards() {
         currentFamily={currentFamily}
         onBrandChange={handleBrandChange}
         onFamilyChange={handleFamilyChange}
+        totalCount={items.length}
+        filteredCount={filteredItems.length}
       />
       <DeviceList items={filteredItems} category="gpu" />
     </div>
